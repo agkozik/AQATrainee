d61cb4ab7753d169c9b9ac21e604348702e40de2
Aleksandr Kozik <agkozik@gmail.com>
2019-11-28 20:00:23 +0300

    Adding 2 object to JSON

--- JsonParsersTrainee/src/main/java/jackson/JacksonParserExample.java
+++ JsonParsersTrainee/src/main/java/jackson/JacksonParserExample.java
@@ -2,7 +2,8 @@ package jackson;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,27 +13,36 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
-@Slf4j
 public class JacksonParserExample {
     private static final String PATH = "C:\\DZ\\AQATrainee\\JsonParsersTrainee\\src\\main\\resources\\";
     private static final String FILETOOBJECT = "json.txt";
     private static final String STRINGTOJSON = "Jackson.json";
-    private static Person p1;
-    private static Person p2;
 
     public static void main(String[] args) {
-        rewriteJSONUsingJackson(parseToObjectUsingJackson());
+        ArrayList<FriendsItem> frl = new ArrayList();
+        List<String> taglist = new ArrayList();
+        ArrayList<Person> people=parseToObjectUsingJackson();
+        Person p1,p2,p3,p4;
+        p1 = new Person("1", "2", 2411341.2, "efg", "rqef", 124, "wafad", false, "dgsgs", frl, taglist, "fav", "al", "111", "222", "333", "44", "555", "11111", 34, "sag", 15.2);
+        p2 = new Person("2", "2", 2411341.2, "efg", "rqef", 124, "wafad", false, "dgsgs", frl, taglist, "fav", "al", "111", "222", "333", "44", "555", "22222", 34, "sag", 15.2);
+        p3=people.get(1);
+        p4=people.get(2);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        people.add(p4);
+        rewriteJSONUsingJackson(people);
     }
 
-    private static ArrayList parseToObjectUsingJackson() {
-        ArrayList personList = new ArrayList<>();
+    private static ArrayList<Person> parseToObjectUsingJackson() {
+        ArrayList<Person> personList = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            personList = objectMapper.readValue(new File(PATH + FILETOOBJECT), ArrayList.class);
+            CollectionType typeReference =
+                    TypeFactory.defaultInstance().constructCollectionType(ArrayList.class, Person.class);
+            personList = objectMapper.readValue(new File(PATH + FILETOOBJECT), typeReference);
             for (Object tmp : personList) {
                 System.out.println(tmp);
-                System.out.println("\n");
-                System.out.println(tmp.getClass().getName());
                 System.out.println("\n");
             }
         } catch (IOException e) {
@@ -41,7 +51,7 @@ public class JacksonParserExample {
         return personList;
     }
 
-    public static void writeJSONUsingJackson(ArrayList<Person> personArrayList) {
+     private static void rewriteJSONUsingJackson(ArrayList<Person> personArrayList) {
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = null;
         try {
@@ -52,30 +62,7 @@ public class JacksonParserExample {
             System.out.println("Ошибка доступа к файлу: " + e.getMessage());
         }
         try {
-            Files.write(Paths.get(PATH + STRINGTOJSON), jsonString.getBytes(), StandardOpenOption.CREATE);
-        } catch (IOException e) {
-            System.out.println("Ошибка доступа к файлу: " + e.getMessage());
-        }
-    }
-
-    private static void rewriteJSONUsingJackson(ArrayList<Person> personArrayList) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        List frl = new ArrayList();
-        List taglist = new ArrayList();
-        p1 = new Person("1", "2", 2411341.2, "efg", "rqef", 124, "wafad", false, "dgsgs", frl, taglist, "fav", "al", "111", "222", "333", "44", "555", "11111", 34, "sag", 15.2);
-        p2 = new Person("2", "2", 2411341.2, "efg", "rqef", 124, "wafad", false, "dgsgs", frl, taglist, "fav", "al", "111", "222", "333", "44", "555", "22222", 34, "sag", 15.2);
-        personArrayList.add(p1);
-        personArrayList.add(p2);
-
-        String jsonString = null;
-        try {
-            jsonString = objectMapper
-                    .writerWithDefaultPrettyPrinter()
-                    .writeValueAsString(personArrayList);
-        } catch (JsonProcessingException e) {
-            System.out.println("Ошибка доступа к файлу: " + e.getMessage());
-        }
-        try {
+            assert jsonString != null;
             Files.write(Paths.get(PATH + STRINGTOJSON), jsonString.getBytes(), StandardOpenOption.CREATE);
         } catch (IOException e) {
             System.out.println("Ошибка доступа к файлу: " + e.getMessage());
