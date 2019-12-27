08cb7d9ce88e1817346db9673e040840e4b32f31
Aleksandr Kozik <agkozik@gmail.com>
2019-12-27 10:04:59 +0300

    Some spases deleted

--- Hibernate/src/main/java/entity/Person.java
+++ Hibernate/src/main/java/entity/Person.java
@@ -1,14 +1,11 @@
 package entity;
 
-import jdk.nashorn.internal.objects.annotations.Constructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.beans.ConstructorProperties;
 
 @Entity
 @Data
