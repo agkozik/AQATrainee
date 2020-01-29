1b5d841823ce6b40f5dd0e78ae7c583a43ca2c3f
Aleksandr Kozik <agkozik@gmail.com>
2020-01-29 17:58:58 +0300

    Selenium Framework (Allure Report Added + ScreenShots)

--- FrameworkTut/src/test/java/com/herokuapp/theinternet/testEngine/TestListener.java
+++ FrameworkTut/src/test/java/com/herokuapp/theinternet/testEngine/TestListener.java
@@ -1,11 +1,33 @@
 package com.herokuapp.theinternet.testEngine;
 
+import io.qameta.allure.Attachment;
 import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener extends TestEngine implements ITestListener {
+
+    // Text attachments for Allure
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    // Text attachments for Allure
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog(String message) {
+        return message;
+    }
+
+    // HTML attachments for Allure
+    @Attachment(value = "{0}", type = "text/html")
+    public static String attachHtml(String html) {
+        return html;
+    }
 
     @Override
     public void onTestStart(ITestResult result) {
@@ -23,6 +45,7 @@ public class TestListener extends TestEngine implements ITestListener {
         log.info("[TEST "+testMethodName+" FAILED]");
         log.info("[Screenshot has been put to the test-output directory]");
         takeScreenshot();
+        saveScreenshotPNG(getDriver());
     }
 
     @Override
